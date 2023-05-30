5e12dc8cb11c7abf1d69d0dfe9871752da011336
EtoKomu <peepoetokomu@gmail.com>
2023-05-30 17:31:49 +0500

    Task 3

---
+++ Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+double a = double.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" является чётным");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" не является чётным");
+}

90fcea43078e7ff5c7b8b9567011f616be3c816f
EtoKomu <peepoetokomu@gmail.com>
2023-05-30 17:57:49 +0500

    Task 4

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int index = 2;
+if (a >= 2)
+{
+    Console.Write(index);
+    index += 2;
+}
+while (index <= a)
+{
+    Console.Write(", ");
+    Console.Write(index);
+    index += 2;
+}

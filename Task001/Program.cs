a0725ea0095650f5031d0985f7e1429930177ca3
EtoKomu <peepoetokomu@gmail.com>
2023-05-30 17:14:11 +0500

    Task 1

---
+++ Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (b > a)
+{
+    max = b;
+}
+Console.Write("Наибольшее из введенных чисел: ");
+Console.WriteLine(max);
